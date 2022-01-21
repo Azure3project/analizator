9e2a6182a6edc01d3719bad5361e3aeda3bbaabc
Ola Kowalczyk <56888996+Olakow@users.noreply.github.com>
2022-01-21 18:35:23 +0100

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -12,7 +12,8 @@ Authenticates your credentials and creates a client.
 '''
 # to trzeba ukryć
 
-subscription_key = '1dab45a2c1ad4a009592ff99c6786e86'
+subscription_key = os.environ["SS"]
+#'1dab45a2c1ad4a009592ff99c6786e86'
 endpoint = 'https://project3-analizator-paragonow.cognitiveservices.azure.com/'
 
 
